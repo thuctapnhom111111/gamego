633fc7470e8d56360c32cfd3013d67e988529b10
MrLolo102 <41060831+MrLolo102@users.noreply.github.com>
2019-04-04 20:34:41 +0700

    Update Board.cs

--- Classes/Board.cs
+++ Classes/Board.cs
@@ -30,4 +30,46 @@ namespace Reversi.Classes
 
         #endregion
 
-      
+       #region Constructors
+
+        public Board()
+            : this(DEFAULT_BOARD_SIZE)
+        { }
+
+        public Board(int boardSize)
+        {
+            this.mBoardSize = boardSize;
+            this.mFieldColors = new DiscColor[boardSize, boardSize];
+
+            for (int rowIndex = 0; rowIndex < boardSize; rowIndex++)            
+            {
+                for (int columnIndex = 0; columnIndex < boardSize; columnIndex++)
+                {
+                    this.mFieldColors[rowIndex, columnIndex] = DiscColor.None;
+                }
+            }
+
+            if (boardSize >= 2)
+            {
+                this.mFieldColors[boardSize / 2 - 1, boardSize / 2 - 1] = DiscColor.White;
+                this.mFieldColors[boardSize / 2, boardSize / 2] = DiscColor.White;
+                this.mFieldColors[boardSize / 2 - 1, boardSize / 2] = DiscColor.Black;
+                this.mFieldColors[boardSize / 2, boardSize / 2 - 1] = DiscColor.Black;
+            }
+        }
+
+        private Board(Board orignalBoard)
+        {
+            this.mBoardSize = orignalBoard.Size;
+            this.mFieldColors = new DiscColor[this.Size, this.Size];
+
+            for (int rowIndex = 0; rowIndex < this.Size; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < this.Size; columnIndex++)
+                {
+                    this.mFieldColors[rowIndex, columnIndex] = orignalBoard[rowIndex, columnIndex];
+                }
+            }
+        }
+
+        #endregion
