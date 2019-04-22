d486286ebf8f133f8ce2e0a5d6cd2ba3e5500e74
MrLolo102 <41060831+MrLolo102@users.noreply.github.com>
2019-04-22 15:31:42 +0700

    Update PlayerProperties.cs

--- Classes/PlayerProperties.cs
+++ Classes/PlayerProperties.cs
@@ -21,5 +21,51 @@ namespace Reversi.Classes
         public PlayerProperties(PlayerType type, string name)
             : this(type, name, 2)
         { }
+        #region Properties
+
+        public override PlayerType Type
+        {
+            get
+            {
+                return PlayerType.Computer;
+            }
+        }
+
+        private int Depth
+        {
+            get
+            {
+                return this.mMaxDepth;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void StartMove()
+        {
+            base.StartMove();
+
+
+
+            Thread threadDoNextMove = new Thread(this.DoNextMove);
+            threadDoNextMove.Start();
+        }
+
+        private void DoNextMove()
+        {
+            MoveSolver solver = new MoveSolver(this, this.Depth);
+            int rowIndex;
+            int columnIndex;
+
+            solver.GetNextMove(out rowIndex, out columnIndex);
+            if (!this.Game.IsStopped && !this.Game.IsPaused && (rowIndex >= 0) && (columnIndex >= 0))
+            {
+                this.Game.Board.SetFieldColor(rowIndex, columnIndex, this.Color);
+            }
+        }
+
+        #endregion
     }
 }
