a8f35d95013fe27917f0e3282672aba81f630f40
HungAnony <hungnevermore1998@gmail.com>
2019-04-23 18:58:47 +0700

    Update PlayerProperties

--- Classes/PlayerProperties.cs
+++ Classes/PlayerProperties.cs
@@ -1,80 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 
 namespace Reversi.Classes
 {
-    class ComputerPlayer : Player
+    [Serializable]
+    public class PlayerProperties
     {
-        #region Static
+        public readonly PlayerType Type;
+        public readonly string Name;
+        public readonly int MaxDepth;
 
-        public static readonly int MinDepth = 2;
-        public static readonly int MaxDepth = 7;
-
-        #endregion
-
-        #region ReadOnly
-
-        private int mMaxDepth;
-
-        #endregion
-
-        #region Constructors
-
-        public ComputerPlayer(Game game, DiscColor color, string name, int maxDepth)
-            : base(game, color, name)
+        public PlayerProperties(PlayerType type, string name, int maxDepth)
         {
-            this.mMaxDepth = maxDepth;
+            this.Type = type;
+            this.Name = name;
+            this.MaxDepth = maxDepth;
         }
 
-        #endregion
-
-        #region Properties
-
-        public override PlayerType Type
-        {
-            get
-            {
-                return PlayerType.Computer;
-            }
-        }
-
-        private int Depth
-        {
-            get
-            {
-                return this.mMaxDepth;
-            }
-        }
-
-        #endregion
-
-        #region Methods
-
-        public override void StartMove()
-        {
-            base.StartMove();
-
-
-
-            Thread threadDoNextMove = new Thread(this.DoNextMove);
-            threadDoNextMove.Start();
-        }
-
-        private void DoNextMove()
-        {
-            MoveSolver solver = new MoveSolver(this, this.Depth);
-            int rowIndex;
-            int columnIndex;
-
-            solver.GetNextMove(out rowIndex, out columnIndex);
-            if (!this.Game.IsStopped && !this.Game.IsPaused && (rowIndex >= 0) && (columnIndex >= 0))
-            {
-                this.Game.Board.SetFieldColor(rowIndex, columnIndex, this.Color);
-            }
-        }
-
-        #endregion
+        public PlayerProperties(PlayerType type, string name)
+            : this(type, name, 2)
+        { }
     }
 }
